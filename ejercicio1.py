f6ab9ad2e37b733d82133310bc1f056db78fd417
Alex Cubero <alexcuar@gmail.com>
2020-10-31 19:02:53 +0100

    Add files via upload

---
+++ ejercicio1.py
@@ -0,0 +1,12 @@
+numero1 = float(input("Introduce primer número:"))
+numero2 = float(input("Introduce segundo número:"))
+if numero1 == numero2:
+    print("Los dos números son iguales")
+else:
+    print("Los dos números son diferentes")
+if numero1 > numero2:
+    print("El primer número es mayor que el segundo")
+if numero2 > numero1:
+    print("El segundo número es mayor que el primero")
+
+print("fin de partido vamos a or ellos")
